429ea98da328813e1aef66a80d6683d24dd5cf83
safwan-kher <eng.safwan.kherallah@gmail.com>
2025-05-09 11:49:22 +0200

    init commit

---
+++ src/main/java/com/inbox/App.java
@@ -0,0 +1,13 @@
+package com.inbox;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
